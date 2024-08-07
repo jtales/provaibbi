998e3735ea9b7b424761ca35e7c117c120df6967
João Tales Luchsinger <jtalesam@gmail.com>
2024-08-07 00:57:33 -0400

    Automation Tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://www.tudocelular.com/',
+    baseUrl: 'https://www.tudocelular.com/compare',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/3-automation/compare.cy.js
@@ -0,0 +1,11 @@
+describe('3 - Automation', () => {
+  it('Faz uma comparação', () => {
+    cy.visit('/')
+    cy.get('[href="/compare/9149.html"] > .item > .plus').click()
+    cy.get('[href="/compare/8442.html"] > .item > .plus').click()
+    cy.get('#comparabtn > a').click()
+    cy.get('#controles_titles > .controles > .sizecompare').should('be.visible')
+    cy.get('.row_titles > :nth-child(3) > :nth-child(1)').should('have.text', 'Custo-benefício')
+
+  });
+})
